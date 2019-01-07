ed943242041c68c39ed0207772980b6696cb4d06
Elle Hallal <daniellehallal@gmail.com>
2019-01-07 10:10:03 +0000

    Create londonWeatherForOneDay function. Test basic passing

--- __tests__/weather_test.js
+++ __tests__/weather_test.js
@@ -1,9 +1,18 @@
 import { Weather } from '../src/weather';
 
-  describe('weather', () => {
+describe('weather', () => {
 
-    it('creates a new instance of Weather', () => {
-      const weather = new Weather();
-      expect(weather instanceof Weather).toEqual(true);
-    });
+  let weather;
+  beforeEach(() => {
+    weather = new Weather();
   });
+
+  it('creates a new instance of Weather', () => {
+    expect(weather instanceof Weather).toEqual(true);
+  });
+
+  it('returns 5', async () => {
+    const londonWeather = await weather.londonWeatherForOneDay();
+    expect(londonWeather).toEqual(5);
+  });
+});
--- src/weather.js
+++ src/weather.js
@@ -1,3 +1,8 @@
 export class Weather {
 
+async londonWeatherForOneDay() {
+    // let result = await Promise.resolve("foo")
+    return 5
+  }
+
 }
