78f70e61287b2c7dc3af0745fd20b1480a859de2
Elle Hallal <daniellehallal@gmail.com>
2019-01-09 11:37:55 +0000

    Weather for following 4 days displays on page

--- src/index.js
+++ src/index.js
@@ -11,12 +11,44 @@ async function londonWeather() {
 }
 
 async function londonWeather5Day() {
-  const london5daytemp = document.getElementById('london-5-day-temp')
-  const london5daydescription = document.getElementById('london-5-day-description')
+  // const london5daytemp = document.getElementById('london-5-day-temp')
+  // const london5daydescription = document.getElementById('london-5-day-description')
   const data = await weather.getForecast();
-  london5daytemp.innerHTML = data[0].temp
-  london5daydescription.innerHTML = data[0].description
+  // london5daytemp.innerHTML = data[0].temp
+  // london5daydescription.innerHTML = data[0].description
+  return data
 }
 
+async function displayForecast() {
+  let forecast = await londonWeather5Day()
+  let test = document.getElementById('test')
+
+  forecast.forEach((obj) => {
+    let info = document.createElement("div")
+
+
+    let dayp = document.createElement("p")
+    dayp.innerHTML = obj.day
+    info.appendChild(dayp);
+
+    let timep = document.createElement("p")
+    timep.innerHTML = obj.time
+    info.appendChild(timep);
+
+
+    let tempp = document.createElement("p")
+    tempp.innerHTML = obj.temp
+    info.appendChild(tempp);
+
+    let descriptionp = document.createElement("p")
+    descriptionp.innerHTML = obj.description
+    info.appendChild(descriptionp)
+
+    test.appendChild(info)
+
+  })
+}
+
+
 londonWeather()
-londonWeather5Day()
+displayForecast()
