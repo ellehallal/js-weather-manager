e930b524b16531172419930a0b55673fae6a9acd
Elle H <itsellej@gmail.com>
2019-01-10 21:43:01 +0000

    Add white background to display-forecast. Day and date display on separately

--- src/index.js
+++ src/index.js
@@ -22,13 +22,20 @@ async function displayTodayWeather() {
 
 async function displayForecast() {
   const forecast = await weather.fourDayForecast();
-  const displayForecast = document.getElementById('display-forecast')
+  const displayForecast = document.getElementById('display-forecast');
 
   forecast.forEach((obj) => {
     let dayForecast = document.createElement('div');
-    let dayForecastHeader = document.createElement('h3');
+    let dayForecastHeader = document.createElement('div');
+    let dayForecastDay = document.createElement('h3');
+    let dayForecastDate = document.createElement('p');
+    let dayForecastHR = document.createElement('hr');
     dayForecast.classList.add('day-forecast');
-    dayForecastHeader.innerHTML = `${obj.day} - ${obj.date}`
+    dayForecastDay.innerHTML = `${obj.day}`;
+    dayForecastDate.innerHTML = `${obj.date}`;
+    dayForecastHeader.appendChild(dayForecastDay);
+    dayForecastHeader.appendChild(dayForecastDate);
+    dayForecastHeader.appendChild(dayForecastHR);
     dayForecast.appendChild(dayForecastHeader);
 
     let objectData = obj.data;
@@ -37,21 +44,31 @@ async function displayForecast() {
       let dayForecastData = document.createElement('div');
       dayForecastData.classList.add('day-forecast-data');
 
+      let time = document.createElement('p');
+      time.classList.add('forecast-time')
+      time.innerHTML = `Time: ${obj.time}`;
+      dayForecastData.appendChild(time);
+
+
+      let forecastIcon = document.createElement('div');
+      forecastIcon.classList.add('forecast-icon');
       let icon = document.createElement('img');
       icon.src = `http://openweathermap.org/img/w/${obj.icon}.png`;
-      dayForecastData.appendChild(icon);
-
-      let time = document.createElement('p');
-      time.innerHTML = obj.time;
-      dayForecastData.appendChild(time);
+      forecastIcon.appendChild(icon);
+      dayForecastData.appendChild(forecastIcon);
 
       let temp = document.createElement('p');
       temp.innerHTML = obj.temp;
       dayForecastData.appendChild(temp);
 
       let desc = document.createElement('p');
+      desc.classList.add('forecast-description')
       desc.innerHTML = obj.description;
       dayForecastData.appendChild(desc);
+
+      let forecastHR = document.createElement('hr');
+      forecastHR.classList.add('forecast-hr');
+      dayForecastData.appendChild(forecastHR);
 
       dayForecast.appendChild(dayForecastData);
 
