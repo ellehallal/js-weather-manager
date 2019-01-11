ba2c63b0d3760bf90cc862c92fe1c997568dbb9f
Elle Hallal <daniellehallal@gmail.com>
2019-01-11 08:53:26 +0000

    Seperate tests based on what they are testing

--- __tests__/weather_test.js
+++ __tests__/weather_test.js
@@ -13,59 +13,72 @@ describe('weather', () => {
     expect(weather).toBeInstanceOf(Weather);
   });
 
-  it('returns London temperature as a string', async () => {
-    const londonWeather = await weather.getOneDayWeather();
-    expect(londonWeather.temp).toContain('°C');
+
+  describe('getOneDayWeather', () => {
+
+    it('returns London temperature as a string', async () => {
+      const londonWeather = await weather.getOneDayWeather();
+      expect(londonWeather.temp).toContain('°C');
+    });
+
+    it('returns description of London weather as a string', async () => {
+      const londonWeather = await weather.getOneDayWeather();
+      expect(typeof londonWeather.description).toEqual('string');
+    });
+
+    it('returns London minimum temperature as a string', async () => {
+      const londonWeather = await weather.getOneDayWeather();
+      expect(londonWeather.mintemp).toContain('°C');
+    });
+
+    it('returns London maximum temperature as a string', async () => {
+      const londonWeather = await weather.getOneDayWeather();
+      expect(londonWeather.maxtemp).toContain('°C');
+    });
+
+    it('returns the location (London) string', async () => {
+      const londonWeather = await weather.getOneDayWeather();
+      expect(londonWeather.location).toContain('London');
+    });
   });
 
-  it('returns description of London weather as a string', async () => {
-    const londonWeather = await weather.getOneDayWeather();
-    expect(typeof londonWeather.description).toEqual('string');
+
+  describe('getDatesAndTimes', () => {
+
+    it('returns an array of 16 dates and times as strings in an array', () => {
+      expect(weather.getDatesAndTimes().length).toEqual(16);
+    });
   });
 
-  it('returns London minimum temperature as a string', async () => {
-    const londonWeather = await weather.getOneDayWeather();
-    expect(londonWeather.mintemp).toContain('°C');
+
+  describe('fourDayForecast', () => {
+
+    it('creates an array containing 4 objects', async () => {
+      const data = await weather.fourDayForecast();
+      expect(data.length).toEqual(4);
+    });
+
+    it('returns the date for the first object in the array', async () => {
+      const data = await weather.fourDayForecast();
+      expect(data[0].date).toContain('/');
+    });
+
+    it('returns the day for the first object in the array', async () => {
+      const data = await weather.fourDayForecast();
+      expect(data[0].day).toContain('day');
+    });
+
+    it('returns the time for the first object in the array', async () => {
+      const data = await weather.fourDayForecast();
+      expect(data[0].data[0].time).toContain(':');
+    });
+
+    it('returns the temp for the first object in the array', async () => {
+      const data = await weather.fourDayForecast();
+      expect(data[0].data[0].temp).toContain('°C');
+    });
   });
 
-  it('returns London maximum temperature as a string', async () => {
-    const londonWeather = await weather.getOneDayWeather();
-    expect(londonWeather.maxtemp).toContain('°C');
-  });
-
-  it('returns the location (London) string', async () => {
-    const londonWeather = await weather.getOneDayWeather();
-    expect(londonWeather.location).toContain('London');
-  });
-
-  it('returns an array of 16 dates and times as strings in an array', () => {
-    expect(weather.getDatesAndTimes().length).toEqual(16);
-  });
-
-  it('creates an array containing 4 objects', async () => {
-    const data = await weather.fourDayForecast();
-    expect(data.length).toEqual(4);
-  });
-
-  it('returns the date for the first object in the array', async () => {
-    const data = await weather.fourDayForecast();
-    expect(data[0].date).toContain('/');
-  });
-
-  it('returns the day for the first object in the array', async () => {
-    const data = await weather.fourDayForecast();
-    expect(data[0].day).toContain('day');
-  });
-
-  it('returns the time for the first object in the array', async () => {
-    const data = await weather.fourDayForecast();
-    expect(data[0].data[0].time).toContain(':');
-  });
-
-  it('returns the temp for the first object in the array', async () => {
-    const data = await weather.fourDayForecast();
-    expect(data[0].data[0].temp).toContain('°C');
-  });
 
   describe('convertDayToDate', () => {
     it('converts the day as an integer to a string with the day name', () => {
@@ -73,11 +86,13 @@ describe('weather', () => {
     });
   });
 
+
   describe('convertZeroTemperature', () => {
     it('converts -0 to 0', () => {
       expect(weather.convertZeroTemperature(-0)).toEqual(0);
     });
   });
+
 
   describe('removeDuplicates', () => {
     it('removes duplicate objects from an array, based on the specified key', () => {
